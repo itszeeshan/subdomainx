3cd23cd681551b61f5d1723e7437ee41bfc320b4
Muhammad Zeeshan <iamzeeshan1233@gmail.com>
2026-07-09 05:47:59 +0500

    feat: update next config

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,4 +1,6 @@
-const withNextra = require('nextra')({
+import nextra from 'nextra'
+
+const withNextra = nextra({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.jsx',
   defaultShowCopyCode: true,
@@ -8,4 +10,4 @@ const withNextra = require('nextra')({
   staticImage: true
 })
 
-module.exports = withNextra()
+export default withNextra()
